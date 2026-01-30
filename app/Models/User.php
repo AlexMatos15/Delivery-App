da827d3df278ddaf03e3b1d41d131a4c55346732
Alex Matos <alexgui31@hotmail.com>
2026-01-30 03:55:56 -0300

    Implement admin authorization: add Gate, update User model fillable, enforce role-based menu visibility

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'type',
+        'is_admin',
+        'is_active',
     ];
 
     /**
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Config;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,11 +27,67 @@ class AppServiceProvider extends ServiceProvider
                 $user = Auth::user();
                 
                 if ($user->isAdmin()) {
-                    // Admin menu - deixa o padrão
+                    // Admin menu - deixa o padrão do config/adminlte.php
                     return;
                 }
                 
-                // Menu para clientes e lojas
+                if ($user->isShop()) {
+                    // Menu para lojas
+                    $shopMenu = [
+                        [
+                            'type' => 'navbar-search',
+                            'text' => 'search',
+                            'topnav_right' => true,
+                        ],
+                        [
+                            'type' => 'fullscreen-widget',
+                            'topnav_right' => true,
+                        ],
+                        ['header' => 'Gestão'],
+                        [
+                            'text' => 'Painel',
+                            'url' => 'dashboard',
+                            'icon' => 'fas fa-home',
+                        ],
+                        [
+                            'text' => 'Pedidos',
+                            'url' => 'orders',
+                            'icon' => 'fas fa-box',
+                        ],
+                        [
+                            'text' => 'Produtos',
+                            'url' => 'admin/products',
+                            'icon' => 'fas fa-cube',
+                        ],
+                        [
+                            'text' => 'Categorias',
+                            'url' => 'products',
+                            'icon' => 'fas fa-list',
+                        ],
+                        ['header' => 'Conta'],
+                        [
+                            'text' => 'Meu Perfil',
+                            'url' => 'my-profile',
+                            'icon' => 'fas fa-user',
+                        ],
+                        [
+                            'text' => 'Alterar Senha',
+                            'url' => 'password',
+                            'icon' => 'fas fa-lock',
+                        ],
+                        [
+                            'text' => 'Sair',
+                            'url' => 'logout',
+                            'icon' => 'fas fa-sign-out-alt',
+                            'onclick' => 'event.preventDefault(); document.getElementById(\'logout-form\').submit();',
+                        ],
+                    ];
+                    
+                    Config::set('adminlte.menu', $shopMenu);
+                    return;
+                }
+                
+                // Menu para clientes
                 $clientMenu = [
                     [
                         'type' => 'navbar-search',
@@ -86,7 +143,7 @@ class AppServiceProvider extends ServiceProvider
                     ],
                 ];
                 
-                config(['adminlte.menu' => $clientMenu]);
+                Config::set('adminlte.menu', $clientMenu);
             }
         });
     }
